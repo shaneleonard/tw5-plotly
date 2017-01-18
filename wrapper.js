55a19155afcabf8697213dfc05b22f47259d23dd
Shane Leonard <shanel@stanford.edu>
2017-01-18 02:10:38 -0800

    Initial commit with somewhat working plugin

---
+++ wrapper.js
@@ -0,0 +1,91 @@
+/*\
+title: $:/plugins/shaneleonard/plotly/wrapper.js
+type: application/javascript
+module-type: widget
+
+A widget for including Plotly charts in a Tiddlywiki.
+
+\*/
+
+(function() {
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+if (!$tw.node) console.log("No node environment");
+else console.log("Node is defined");
+
+var Plotly = require("$:/plugins/shaneleonard/plotly/plotly-latest.js");
+console.log(Plotly);
+
+var PlotlyWidget = function(parseTreeNode,options) {
+    this.initialise(parseTreeNode,options);
+};
+
+/*
+ * Inherit from the base widget class
+ */
+PlotlyWidget.prototype = new Widget();
+
+/*
+ * Render this widget into the DOM
+ */
+PlotlyWidget.prototype.render = function(parent, nextSibling) {
+    /* Save the parent dom node */
+    this.parentDomNode = parent;
+
+    /* Compute our attributes */
+    this.computeAttributes();
+
+    this.execute();
+
+    /* Create the chart */
+    var plot = this.createPlot();
+
+    /* Insert the plot into the DOM */
+    parent.insertBefore(plot.domNode,nextSibling);
+    this.domNodes.push(plot.domNode);
+
+    this.updatePlot = plot.updatePlot;
+
+    if (this.updatePlot) {
+        this.updatePlot();
+    }
+
+};
+
+PlotlyWidget.prototype.createPlot = function(){
+    var div = document.createElement("div");
+    this.plotDiv = div;
+    return {
+        domNode: div,
+        updatePlot: function() { Plotly.newPlot(div, this.plotData, this.plotLayout ? this.plotLayout : {autosize: true}); }
+    }
+};
+
+/*
+ * Compute the widget attributes
+ */
+PlotlyWidget.prototype.execute = function() {
+    var d = this.getAttribute("data");
+    this.plotData = this.wiki.getTiddlerData(d);
+
+    if (this.hasAttribute("layout")) {
+        var l = this.getAttribute("layout");
+        this.plotLayout = this.wiki.getTiddlerData(l);
+    }
+};
+
+/*
+Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
+*/
+PlotlyWidget.prototype.refresh = function(changedTiddlers) {
+    Plotly.Plots.resize(this.plotDiv);
+	return true;
+};
+
+exports.plotly = PlotlyWidget;
+
+})();
